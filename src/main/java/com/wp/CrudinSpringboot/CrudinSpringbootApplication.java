1a3826f0a3b655b32d9c5e83f019277f8f01a95a
laadp834 <pooja.laad@fisglobal.com>
2020-10-29 20:18:59 +0530

    Spring boot Crud for jenkins job

---
+++ src/main/java/com/wp/CrudinSpringboot/CrudinSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wp.CrudinSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudinSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudinSpringbootApplication.class, args);
+	}
+
+}
